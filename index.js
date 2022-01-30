5db625553a2523f53d4fe0cf1399489e95702e28
rahulsinghrs2512 <96308524+rahulsinghrs2512@users.noreply.github.com>
2022-01-30 17:26:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//LOVE CALCULATOR
+var a=prompt("Enter your name");
+
+var b=prompt("enter your crush name");
+var r=Math.random();
+r=r*100;
+r=Math.floor(r);
+ 
+alert(a+" your match is "+r+"% with your crush "+b);
